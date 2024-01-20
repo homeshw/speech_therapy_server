04ff6fe28c497ba1395310717f1ed9534f0b70ac
Homesh Wathsalya Ampe Mohottige <homeshw@Homeshs-MacBook-Pro.local>
2024-01-20 22:40:16 +0530

    wav to mp3

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const multer = require('multer');
 const path = require('path');
 const cors = require('cors'); 
+const ffmpegPath = require('@ffmpeg-installer/ffmpeg').path;
+const ffmpeg = require('fluent-ffmpeg');
+ffmpeg.setFfmpegPath(ffmpegPath);
 
 const app = express();
 const port = 5001;
@@ -12,6 +15,9 @@ app.use(cors({
 }));
 
 const upload_loc = '../speech_therapy/src/audio_clips'
+
+const inputPath = path.join(upload_loc,'voice.wav');
+const outputPath = path.join(upload_loc,'voice.mp3');
 
 // Set up Multer to handle file uploads
 const storage = multer.diskStorage({
@@ -26,11 +32,36 @@ const upload = multer({ storage: storage });
 // Serve the React app
 app.use(express.static(path.join(__dirname, 'speech_therapy/build')));
 
+function convertWavToMp3(inputPath, outputPath) {
+  return new Promise((resolve, reject) => {
+    ffmpeg()
+      .input(inputPath)
+      .audioCodec('libmp3lame')
+      .toFormat('mp3')
+      .on('end', () => {
+        console.log('Conversion finished');
+        resolve();
+      })
+      .on('error', (err) => {
+        console.error('Error:', err);
+        reject(err);
+      })
+      .save(outputPath);
+  });
+}
+
 // Handle file upload endpoint
 app.post('/upload', upload.single('file'), (req, res) => {
   console.log(req)
   console.log('File uploaded:', req.file);
   res.status(200).send('File uploaded successfully!');
+  convertWavToMp3(inputPath, outputPath)
+  .then(() => {
+    console.log('Conversion successful');
+  })
+  .catch((error) => {
+    console.error('Conversion failed:', error);
+  });
 });
 
 // Catch-all route to serve the React app
